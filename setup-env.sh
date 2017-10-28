62b9014c322a989deaede99f2535663002467632
Joseph deBlaquiere <jadeblaquiere@yahoo.com>
2017-10-28 14:38:17 +0000

    shell script to install prerequisite libraries

---
+++ setup-env.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+#install gmp
+echo "INSTALLING GMP LIBRARIES"
+sudo apt-get install libgmp-dev
+
+echo "INSTALLING PBC PREREQUISITES"
+#install pbc
+sudo apt-get install build-essential flex bison
+echo "BUILDING PBC LIBRARIES"
+git clone https://github.com/blynn/pbc.git
+cd pbc
+sh ./setup
+./configure --prefix=/usr --enable-shared
+make
+echo "INSTALLING PBC LIBRARIES"
+sudo make install
+cd ..
+
+echo "INSTALLING PBC PYTHON BINDINGS"
+sudo pip3 install git+https://github.com/debatem1/pypbc.git
