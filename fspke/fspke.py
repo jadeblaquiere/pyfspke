70852dec683b40336f32c3b649e0698dd1474b99
Joseph deBlaquiere <jadeblaquiere@yahoo.com>
2017-11-07 21:11:43 +0000

    support ASN.1 DER encoding of ciphertext

--- fspke/fspke.py
+++ fspke/fspke.py
@@ -27,12 +27,13 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from Crypto.Random import random
-import rabinmiller as rabinmiller
+import fspke.rabinmiller as rabinmiller
 from pypbc import *
-from simplebtree import SimpleNTree
-from icarthash import IcartHash
+from fspke.simplebtree import SimpleNTree
+from fspke.icarthash import IcartHash
 from binascii import hexlify, unhexlify
 import json
+import asn1
 
 """fspke implements a cryptosystem based on the Canetti, Halevi and Katz
    model as defined in "A Forward-Secure Public-Key Encryption Scheme",
@@ -107,7 +108,7 @@ class CHKPublicKey (object):
         self.Q = Element(self.pairing, G1, value=pubkey['Q'])
         self._H = IcartHash.deserialize(pubkey['H'])
         self.H = self._hashFunc
-        self.gt = self.pairing.apply(self.P, pke.Q)
+        self.gt = self.pairing.apply(self.P, self.Q)
         self.tree = SimpleNTree(self.order, init=CHKPublicKey._btree_init)
         self.eQH = self.pairing.apply(self.Q, self.H(self.tree.nodeId()))
 
@@ -129,8 +130,18 @@ class CHKPublicKey (object):
         node.R = []
         node.S = None
 
-    def _hashFunc(self,x):
-        """hashfunc uses the Icart hash function and converts it to Element
+    def _hexprint_q(self, x):
+        """_hexprint_q prints a zero-padded fixed length hex representation
+        based on the bit size of q
+        """
+        pfmt = '%%0%dx' % (int((self.q.bit_length() + 7) // 8) * 2)
+        return (pfmt % x)
+
+    def _byterep_q(self,x):
+        return unhexlify(self._hexprint_q(x))
+
+    def _hashFunc(self, x):
+        """_hashfunc uses the Icart hash function and converts it to Element
         """
         h = self._H.hashval(x)
         if h[1] == True:
@@ -152,25 +163,55 @@ class CHKPublicKey (object):
 
     def Enc(self, M, interval, lam=None):
         """Enc is the encryption function which takes a single element
-           and encrypts it using the encryption key for specific interval
+        and encrypts it using the encryption key for specific interval
         """
+        ctraw = self._enc(M, interval, lam)
+        ct = []
+        for c in ctraw:
+            ct.append(str(c))
+        return ct
+
+    def Enc_DER(self, M, interval, lam=None):
+        """Enc is the encryption function which takes a single element
+        and encrypts it using the encryption key for specific interval. 
+        Enc_DER produces output in ASN.1 DER binary format
+        """
+        ctraw = self._enc(M, interval, lam)
+        encoder = asn1.Encoder()
+        encoder.start()
+        encoder.enter(asn1.Numbers.Sequence)
+        for c in ctraw[:-1]:
+            byteval = unhexlify(str(c))
+            encoder.write(byteval, asn1.Numbers.OctetString)
+        encoder.enter(asn1.Numbers.Sequence)
+        Md = ctraw[-1]
+        for n in range(0,2):
+            byteval = self._byterep_q(Md[n])
+            encoder.write(byteval, asn1.Numbers.OctetString)
+        encoder.leave()
+        encoder.leave()
+        return encoder.output()
+
+    def _enc(self, M, interval, lam=None):
+        # internal encryption function - returns list of Element types
+        # which encode the ciphertext
         # assume M in GT
         # lambda is a python reserved word, so here lam means lambda
         if lam is None:
             lam = Element.random(self.pairing, Zr)
         hlist = self._hashlist(self.depth, interval)
-        Ct = []
-        Ct.append(str(self.P * lam))
+        ct = []
+        ct.append(self.P * lam)
         for H in hlist:
             # print("type,h = ", type(h), h)
             # H = self.P * h
             # print("type,H = ", type(H), H)
-            Ct.append(str(H * lam))
+            ct.append(H * lam)
         d = pow(self.eQH, lam)
         # print("type,m = ", type(m), m)
         # print("type,d = ", type(d), d)
-        Ct.append(str(M * d))
-        return Ct
+        ct.append(M * d)
+        return ct
 
 
 class CHKPrivateKey (CHKPublicKey):
@@ -218,12 +259,8 @@ class CHKPrivateKey (CHKPublicKey):
         self._validateParams()
         # self.cwH = CWHashFunction(self.q)
         pointFmtCompressed = PBC_EC_Compressed
-        set_point_format_uncompressed()
-        Pstr = str(self.P)[2:]
-        G = (int(Pstr[:len(Pstr)//2], 16), int(Pstr[len(Pstr)//2:], 16))
-        print("Generator = ", G)
-        if pointFmtCompressed != 0:
-            set_point_format_compressed()
+        G = (self.P[0], self.P[1])
+        # print("Generator = ", G)
         self._H = IcartHash(self.q, 1, 0, G, self.r)
         self.H = self._hashFunc
         self.tree = SimpleNTree(self.order, init=CHKPublicKey._btree_init)
@@ -294,6 +331,9 @@ class CHKPrivateKey (CHKPublicKey):
         self._clearUp(node)
 
     def _deriveRightAndUpRight(self, node):
+        # traverses the nodes "forward in time" - to the right amongst peers
+        # and then right-peers of parent (and recursively up) - to derive
+        # keys needed for "the future"
         if node.parent is None:
             return []
         # ensure peers of later interval (right) have derived their keys
@@ -306,7 +346,7 @@ class CHKPrivateKey (CHKPublicKey):
         return keylist
 
     def _clearLeftandDown(self, node):
-        print("clearLeftandDown called for ", str(node))
+        # traverses tree to erase secret keys for "back in time"
         if node.parent is None:
             return
         # loop over peers, 
@@ -314,11 +354,13 @@ class CHKPrivateKey (CHKPublicKey):
             if c.nodeId() < node.nodeId():
                 c.S = None
                 c.R = []
+                # no need to descend and erase keys - prune the whole subtree
                 c.pruneChildren()
         self._clearLeftandDown(node.parent)
 
     def _clearUp(self, node):
-        print("clearUp called for ", str(node))
+        # clears secret keys for parent node elements - back to the root
+        # any parent node can derive keys for children
         parent = node.parent
         if parent is not None:
             parent.S = None
@@ -330,13 +372,56 @@ class CHKPrivateKey (CHKPublicKey):
         the message value (which is a point in Fp2) using the key for the
         specific interval.
         """
-        # importing the ciphertext as strings handles case
-        # of strings or where points are coming from distinct
-        # pairing object (e.g. test code with multiple pkes)
         C = []
         for Ct in Ctin[0:-1]:
             C.append(Element(self.pairing, G1, value=str(Ct)))
         C.append(Element(self.pairing, GT, value=str(Ctin[-1])))
+        return self._dec(C, interval)
+        
+    def Dec_DER(self, CtinDER, interval):
+        """Dec is the decryption function which translates a ciphertext into
+        the message value (which is a point in Fp2) using the key for the
+        specific interval.
+        Dec_DER takes ciphertext input in ASN.1 DER binary format
+        """
+        # importing the ciphertext as strings handles case
+        # of strings or where points are coming from distinct
+        # pairing object (e.g. test code with multiple pkes)
+        decoder = asn1.Decoder()
+        decoder.start(CtinDER)
+        tag = decoder.peek()
+        if tag.nr != asn1.Numbers.Sequence:
+            raise ValueError("Unexpected DER tag")
+        decoder.enter()
+        C = []
+        tag = decoder.peek()
+        while tag.nr != asn1.Numbers.Sequence:
+            if tag.nr != asn1.Numbers.OctetString:
+                raise ValueError("Unexpected DER tag")
+            tag, val = decoder.read()
+            C.append(Element(self.pairing, G1, value=hexlify(val).decode()))
+            tag = decoder.peek()
+        decoder.enter()
+        tag = decoder.peek()
+        if tag.nr != asn1.Numbers.OctetString:
+            raise ValueError("Unexpected DER tag")
+        tag, valx = decoder.read()
+        tag = decoder.peek()
+        if tag.nr != asn1.Numbers.OctetString:
+            raise ValueError("Unexpected DER tag")
+        tag, valy = decoder.read()
+        decoder.leave()
+        decoder.leave()
+        if decoder.eof() != True:
+            raise ValueError("Unexpected DER tag")
+        Md = "(0x" + hexlify(valx).decode() + ", 0x" + hexlify(valy).decode() + ")"
+        C.append(Element(self.pairing, GT, value=str(Md)))
+        return self._dec(C, interval)
+        
+    def _dec(self, C, interval):
+        # internal decryption function takes a list of elements (depth + 1
+        # points in G1 followed by x,y coordinates in pairing target group)
+        # and recovers the original (target group) message point
         U0 = C[0]
         U = C[1:-1]
         V = C[-1]
@@ -356,121 +441,3 @@ class CHKPrivateKey (CHKPublicKey):
         us = self.pairing.apply(U0, S)
         d = us * pow(pi, self.r - 1)
         return V * pow(d, self.r - 1)
-
-if __name__ == '__main__':
-    set_point_format_compressed()
-    pke = CHKPrivateKey(256, 200, 6, order=16)
-    print("params =", pke.params)
-    print("q =", pke.q)
-    print("h =", pke.h)
-    print("r =", pke.r)
-    print("pairing =", pke.pairing)
-    print("P =", str(pke.P))
-    print("Q =", str(pke.Q))
-    print("gt =", str(pke.gt))
-    print("H =", str(pke.H))
-    SK0 = pke._der(0,0)
-    print("SK0 = ", SK0)
-    SK10 = pke._der(1,0)
-    print("SK10 = ", SK10)
-    SK11 = pke._der(1,1)
-    print("SK11 = ", SK11)
-    SK20 = pke._der(2,0)
-    print("SK20 = ", SK20)
-    SK21 = pke._der(2,1)
-    print("SK21 = ", SK21)
-    SK22 = pke._der(2,2)
-    print("SK22 = ", SK22)
-    SK23 = pke._der(2,3)
-    print("SK23 = ", SK23)
-    # export/import pubkey
-    pubkey = pke.publicKeyToJSON()
-    pke2 = CHKPublicKey.publicKeyFromJSON(pubkey)
-    SK123456 = pke.Der(0x123456)
-    SK56789A = pke.Der(0x56789A)
-    print("key(0x123456) = ", str(SK123456))
-    print("key(0x56789A) = ", str(SK56789A))
-    # encrypt a message
-    for i in range(0,10):
-        m = random.randint(1,pke.r)
-        me = pke.gt * m
-        print("Random message = 0x%X" % (m))
-        print("Random element = ", str(me))
-        lam = Element.random(pke.pairing, Zr)
-        C = pke2.Enc(me,0x123456,lam)
-        C2 = pke2.Enc(me,0x56789A,lam)
-        C1 = pke.Enc(me,0x123456,lam)
-        C3 = pke.Enc(me,0x56789A,lam)
-        print("ciphertext 1  = ", str(C))
-        print("ciphertext 2  = ", str(C2))
-        assert str(C) == str(C1)
-        assert str(C2) == str(C3)
-        if True:
-            d = pke.Dec(C,0x123456)
-            dn = pke.Dec(C,0x56789A)
-            d2 = pke.Dec(C3,0x56789A)
-            d2n = pke.Dec(C3,0x123456)
-            print("decrypted 1 = ", str(d))
-            print("decrypted 2 = ", str(d2))
-            print("decrypted 1n = ", str(dn))
-            print("decrypted 2n = ", str(d2n))
-            assert d == me
-            assert dn != me
-            assert d2 == me
-            assert d2n != me
-    print("forgetting before 0x200000")
-    pke.ForgetBefore(0x200000)
-    keyset = pke.ExportKeyset(0x200000)
-    for k in keyset:
-        print("key @ %s = %s" % (k[0], k[1]))
-    print("forgotten")
-    SKx123456 = pke.Der(0x123456)
-    SKx56789A = pke.Der(0x56789A)
-    assert SKx123456 is None
-    assert SKx56789A == SK56789A
-    a = Element.random(pke.pairing, Zr)
-    b = Element.random(pke.pairing, Zr)
-    # pbc treats * and ** both as the group operation (scalar multiplication)
-    # for elliptic curve points
-    g1a = pke.P * a
-    g2b = pke.Q * b
-    g1aExp = pow(pke.P, a)
-    g2bExp = pow(pke.Q, b)
-    assert g1a == g1aExp
-    assert g2b == g2bExp
-    # the opposite bilinear mapping
-    g1b = pke.P * b
-    g2a = pke.Q * a
-    bilin = pke.pairing.apply(g1a, g2b)
-    bili2 = pke.pairing.apply(g1b, g2a)
-    check = pow(pke.gt, (a*b))
-    print("bilin = ", str(bilin))
-    print("check = ", str(check))
-    print("bili2 = ", str(bili2))
-    assert bilin == check
-    assert bilin == bili2
-    # test order, cofactor
-    rm1 = pke.r - 1 
-    rm1P = pow(pke.P,rm1)
-    rp1 = pke.r + 1 
-    rp1P = pow(pke.P,rp1)
-    z = pke.P + rm1P
-    print("P =", str(pke.P))
-    print("(r-1)P =", str(rm1P))
-    print("(r+1)P =", str(rp1P))
-    print("z =", str(z))
-    pcheck = pke.pairing.apply(rm1P, pke.Q)
-    print("e(P,q)      = ", str(pke.gt))
-    print("e((r-1)P,q) = ", str(pcheck))
-    print(" sum(above) = ", str(pcheck + pke.gt))
-    gtm1 = pow(pke.gt, rm1)
-    print("gt       =", str(pke.gt))
-    print("gt ** -1 =", str(gtm1))
-    gtone = pke.gt * gtm1
-    print("gt * gti =", str(gtone))
-    r5 = Element(pke.pairing, Zr, value=5)
-    gtfive = pow(pke.gt,r5)
-    print("5 = ", str(r5))
-    print("gt ** 5 = ", str(gtfive))
-    gtfiveinv = gtfive * gtm1
-    print("(gt * 5) * gt ** -1 = ", str(gtfiveinv))
---
+++ tests/test-fspke.py
@@ -0,0 +1,157 @@
+# Copyright (c) 2017, Joseph deBlaquiere <jadeblaquiere@yahoo.com>
+# All rights reserved
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of ecpy nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from Crypto.Random import random
+from pypbc import *
+from fspke.fspke import *
+from fspke.simplebtree import SimpleNTree
+from fspke.icarthash import IcartHash
+
+if __name__ == '__main__':
+    set_point_format_compressed()
+    pke = CHKPrivateKey(256, 200, 6, order=16)
+    print("params =", pke.params)
+    print("q =", pke.q)
+    print("q bitsize =", pke.q.bit_length())
+    print("h =", pke.h)
+    print("r =", pke.r)
+    print("r hex = %X" % (pke.r))
+    print("pairing =", pke.pairing)
+    print("P =", str(pke.P))
+    print("Q =", str(pke.Q))
+    print("gt =", str(pke.gt))
+    print("H =", str(pke.H))
+    SK0 = pke._der(0,0)
+    print("SK0 = ", SK0)
+    SK10 = pke._der(1,0)
+    print("SK10 = ", SK10)
+    SK11 = pke._der(1,1)
+    print("SK11 = ", SK11)
+    SK20 = pke._der(2,0)
+    print("SK20 = ", SK20)
+    SK21 = pke._der(2,1)
+    print("SK21 = ", SK21)
+    SK22 = pke._der(2,2)
+    print("SK22 = ", SK22)
+    SK23 = pke._der(2,3)
+    print("SK23 = ", SK23)
+    # export/import pubkey
+    pubkey = pke.publicKeyToJSON()
+    pke2 = CHKPublicKey.publicKeyFromJSON(pubkey)
+    SK123456 = pke.Der(0x123456)
+    SK56789A = pke.Der(0x56789A)
+    print("key(0x123456) = ", str(SK123456))
+    print("key(0x56789A) = ", str(SK56789A))
+    # encrypt a message
+    for i in range(0,10):
+        m = random.randint(1,pke.r)
+        me = pke.gt * m
+        print("Random message = 0x%X" % (m))
+        print("Random element = ", str(me))
+        lam = Element.random(pke.pairing, Zr)
+        C = pke2.Enc(me,0x123456,lam)
+        C2 = pke2.Enc(me,0x56789A,lam)
+        C1 = pke.Enc(me,0x123456,lam)
+        C3 = pke.Enc(me,0x56789A,lam)
+        C4 = pke.Enc_DER(me,0x56789A,lam)
+        print("ciphertext 1  = ", str(C))
+        print("ciphertext 2  = ", str(C2))
+        print("ciphertext 4  = ", hexlify(C4))
+        assert str(C) == str(C1)
+        assert str(C2) == str(C3)
+        if True:
+            d = pke.Dec(C,0x123456)
+            dn = pke.Dec(C,0x56789A)
+            d2 = pke.Dec(C3,0x56789A)
+            d2n = pke.Dec(C3,0x123456)
+            d4 = pke.Dec_DER(C4,0x56789A)
+            print("decrypted 1 = ", str(d))
+            print("decrypted 2 = ", str(d2))
+            print("decrypted 1n = ", str(dn))
+            print("decrypted 2n = ", str(d2n))
+            assert d == me
+            assert dn != me
+            assert d2 == me
+            assert d2n != me
+    print("forgetting before 0x200000")
+    pke.ForgetBefore(0x200000)
+    keyset = pke.ExportKeyset(0x200000)
+    for k in keyset:
+        print("key @ %s = %s" % (k[0], k[1]))
+    print("forgotten")
+    SKx123456 = pke.Der(0x123456)
+    SKx56789A = pke.Der(0x56789A)
+    assert SKx123456 is None
+    assert SKx56789A == SK56789A
+    a = Element.random(pke.pairing, Zr)
+    b = Element.random(pke.pairing, Zr)
+    # pbc treats * and ** both as the group operation (scalar multiplication)
+    # for elliptic curve points
+    g1a = pke.P * a
+    g2b = pke.Q * b
+    g1aExp = pow(pke.P, a)
+    g2bExp = pow(pke.Q, b)
+    assert g1a == g1aExp
+    assert g2b == g2bExp
+    # the opposite bilinear mapping
+    g1b = pke.P * b
+    g2a = pke.Q * a
+    bilin = pke.pairing.apply(g1a, g2b)
+    bili2 = pke.pairing.apply(g1b, g2a)
+    check = pow(pke.gt, (a*b))
+    print("bilin = ", str(bilin))
+    print("check = ", str(check))
+    print("bili2 = ", str(bili2))
+    assert bilin == check
+    assert bilin == bili2
+    # test order, cofactor
+    rm1 = pke.r - 1 
+    rm1P = pow(pke.P,rm1)
+    rp1 = pke.r + 1 
+    rp1P = pow(pke.P,rp1)
+    z = pke.P + rm1P
+    print("P =", str(pke.P))
+    print("(r-1)P =", str(rm1P))
+    print("(r+1)P =", str(rp1P))
+    print("z =", str(z))
+    pcheck = pke.pairing.apply(rm1P, pke.Q)
+    print("e(P,q)      = ", str(pke.gt))
+    print("e((r-1)P,q) = ", str(pcheck))
+    print(" sum(above) = ", str(pcheck + pke.gt))
+    gtm1 = pow(pke.gt, rm1)
+    print("gt       =", str(pke.gt))
+    print("gt ** -1 =", str(gtm1))
+    gtone = pke.gt * gtm1
+    print("gt * gti =", str(gtone))
+    r5 = Element(pke.pairing, Zr, value=5)
+    gtfive = pow(pke.gt,r5)
+    print("5 = ", str(r5))
+    print("gt ** 5 = ", str(gtfive))
+    gtfiveinv = gtfive * gtm1
+    print("(gt * 5) * gt ** -1 = ", str(gtfiveinv))
+
